d8a73019c61c8f531d57833a5bf69c0ac79eca5f
Sebastian Borza <sebito91@gmail.com>
2023-09-24 23:20:15 -0500

    feat(crawl): getURLsFromHTML initial add

--- webcrawler/crawl.js
+++ webcrawler/crawl.js
@@ -1,3 +1,5 @@
+const {JSDOM} = require('jsdom');
+
 function normalizeURL(url) {
   let newURL = new URL(url);
   let returnURL = `${newURL.hostname}`;
@@ -13,6 +15,24 @@ function normalizeURL(url) {
   return returnURL;
 }
 
+function getURLsFromHTML(htmlBody, baseURL) {
+  const htmlDOM = new JSDOM(htmlBody, {
+    url: `${baseURL}`,
+    contentType: 'text/html',
+    includeNodeLocations: true,
+  });
+
+  let found_urls = [];
+  urls_found = htmlDOM.window.document.querySelectorAll('a');
+  for (let i = 0; i < urls_found.length; i++) {
+    console.log(`SEBTEST -- we found the following at ${i}: ${urls_found[i]}`);
+    found_urls.push(`${urls_found[i]}`);
+  }
+
+  return found_urls;
+}
+
 module.exports = {
   normalizeURL,
+  getURLsFromHTML,
 };
--- webcrawler/crawl.test.js
+++ webcrawler/crawl.test.js
@@ -1,30 +1,53 @@
 const {test, expect} = require('@jest/globals');
 const {normalizeURL} = require('./crawl.js');
+const {getURLsFromHTML} = require('./crawl.js');
 
-test('normal URL + normal path', () => {
+// tests for the normalizeURL function
+test('normalizeURL: normal URL + normal path', () => {
   expect(normalizeURL('https://this.is.a.test.com/tester')).toBe(
     'this.is.a.test.com/tester',
   );
 });
 
-test('normal URL + normal path + trailing /', () => {
+test('normalizeURL: normal URL + normal path + trailing /', () => {
   expect(normalizeURL('https://this.is.a.test.com/tester/')).toBe(
     'this.is.a.test.com/tester',
   );
 });
 
-test('normal URL + normal path + query', () => {
+test('normalizeURL: normal URL + normal path + query', () => {
   expect(
     normalizeURL('https://this.is.a.test.com/tester?tester=a&newtester=b'),
   ).toBe('this.is.a.test.com/tester');
 });
 
-test('normal URL + diff port + normal path', () => {
+test('normalizeURL: normal URL + diff port + normal path', () => {
   expect(normalizeURL('https://this.is.a.test.com:9191/tester')).toBe(
     'this.is.a.test.com:9191/tester',
   );
 });
 
-test('normal URL + no path/query', () => {
+test('normalizeURL: normal URL + no path/query', () => {
   expect(normalizeURL('https://this.is.a.test.com')).toBe('this.is.a.test.com');
+});
+
+// tests for the getURLsFromHTML function
+
+test('getURLsFromHTML: found a single basic href', () => {
+  expect(
+    getURLsFromHTML(
+      `<a href="https://boot.dev">Learn Backend Development</a>`,
+      `https://boot.dev`,
+    ),
+  ).toStrictEqual(['https://boot.dev/']);
+});
+
+test('getURLsFromHTML: found two basic href', () => {
+  expect(
+    getURLsFromHTML(
+      `<a href="https://boot.dev">Learn Backend Development</a>
+      <a href="https://boot.org">Learn Backend Development Org</a>`,
+      `https://boot.dev`,
+    ),
+  ).toStrictEqual(['https://boot.dev/', 'https://boot.org/']);
 });
