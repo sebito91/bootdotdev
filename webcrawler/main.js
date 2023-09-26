daff4b49c4422d19f8b200c2e3c729cca285ab14
Sebastian Borza <sebito91@gmail.com>
2023-09-25 22:43:17 -0500

    feat(main): initial commit

--- webcrawler/main.js
+++ webcrawler/main.js
@@ -1,1 +1,13 @@
-console.log('Hello, world!');
+const {argv} = require('node:process');
+
+function main() {
+  if (argv.length != 3) {
+    throw new Error(
+      `expected a single baseURL, got ${argv.length - 2} args instead`,
+    );
+  }
+
+  console.log(`Calling the crawler for ${argv[2]}...`);
+}
+
+main();
