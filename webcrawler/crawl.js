85e49a2ea1797a3ed69bc7822d4008c8a93e4e2e
Sebastian Borza <sebito91@gmail.com>
2023-09-30 15:37:15 -0500

    feat(crawl, main): update crawlPage to be recursive

--- webcrawler/crawl.js
+++ webcrawler/crawl.js
@@ -2,7 +2,7 @@ const {JSDOM} = require('jsdom');
 
 function normalizeURL(url) {
   let newURL = new URL(url);
-  let returnURL = `${newURL.hostname}`;
+  let returnURL = `${newURL.protocol}//${newURL.hostname}`;
 
   if (newURL.port) {
     returnURL = returnURL.concat(`:${newURL.port}`);
@@ -24,37 +24,63 @@ function getURLsFromHTML(htmlBody, baseURL) {
 
   let found_urls = [];
   urls_found = htmlDOM.window.document.querySelectorAll('a');
-
-  return found_urls;
-}
-
-async function crawlPage(baseURL) {
-  const resp = await fetch(baseURL);
-
-  if (!resp.ok) {
-    //throw new Error(`received error code ${resp.status} for ${baseURL}`);
-    console.log(`received error code ${resp.status} for ${baseURL}`);
-    return;
-  }
-
-  if (!resp.headers.get('Content-Type').includes('text/html')) {
-    //throw new Error(`received different Content-Type header ${resp.headers.get('Content-Type')}, expected 'text/html'`);
-    console.log(
-      `received different Content-Type header ${resp.headers.get(
-        'Content-Type',
-      )}, expected 'text/html'`,
-    );
-    return;
-  }
-
-  const htmlText = await resp.text();
-  found_urls = getURLsFromHTML(htmlText, baseURL);
   for (let i = 0; i < urls_found.length; i++) {
-    console.log(`SEBTEST -- we found the following at ${i}: ${urls_found[i]}`);
     found_urls.push(`${urls_found[i]}`);
   }
 
   return found_urls;
+}
+
+async function crawlPage(baseURL, currentURL, pages) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  if (!currentURL.startsWith(baseURL)) {
+    return pages;
+  }
+
+  const currentURL_normalized = normalizeURL(currentURL);
+
+  // if we already exist in the pages object, increment and return
+  // else if we are the same as the baseURL we set to 0
+  // else we set the currentURL to 1
+  if (currentURL_normalized in pages) {
+    pages[currentURL_normalized]++;
+    return pages;
+  } else if (baseURL == currentURL) {
+    pages[currentURL_normalized] = 0;
+  } else {
+    pages[currentURL_normalized] = 1;
+  }
+
+  const resp = await fetch(currentURL_normalized);
+  if (!resp.ok) {
+    console.log(
+      `received error code ${resp.status} for ${currentURL_normalized}`,
+    );
+    return pages;
+  }
+
+  if (!resp.headers.get('Content-Type').includes('text/html')) {
+    console.log(
+      `received different Content-Type header from ${currentURL_normalized}, got ${resp.headers.get(
+        'Content-Type',
+      )}, expected 'text/html'`,
+    );
+    return pages;
+  }
+
+  const htmlText = await resp.text();
+  found_urls = getURLsFromHTML(htmlText, baseURL);
+
+  for (let i = 0; i < found_urls.length; i++) {
+    try {
+      pages = await crawlPage(baseURL, found_urls[i], pages);
+    } catch (error) {
+      console.log(`${error}`);
+    }
+  }
+
+  return pages;
 }
 
 module.exports = {
--- webcrawler/main.js
+++ webcrawler/main.js
@@ -1,15 +1,29 @@
 const {argv} = require('node:process');
 const {crawlPage} = require('./crawl.js');
 
-function main() {
+async function startCrawl(baseURL, currentURL) {
+  const pages = await crawlPage(baseURL, currentURL, {});
+  return pages;
+}
+
+async function main() {
   if (argv.length != 3) {
     throw new Error(
       `expected a single baseURL, got ${argv.length - 2} args instead`,
     );
   }
 
-  console.log(`Calling the crawler for ${argv[2]}...`);
-  crawlPage(argv[2]);
+  const baseURL = argv[2];
+  const currentURL = argv[2];
+
+  console.log(`Calling the crawler for ${baseURL}...`);
+
+  const pages = await crawlPage(baseURL, currentURL, {});
+  const pageset = Object.keys(pages);
+
+  pageset.forEach((page) => {
+    console.log(`Found ${pages[page]} visits to ${page}`);
+  });
 }
 
 main();
