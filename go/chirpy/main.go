74d1880503a8645c56f79c80a4b63936e769ce0f
Sebastian Borza <sebito91@gmail.com>
2023-11-13 21:42:55 -0600

    feat(chirpy): add logo and healthz handler

--- go/chirpy/main.go
+++ go/chirpy/main.go
@@ -19,12 +19,24 @@ func middlewareCors(next http.Handler) http.Handler {
 	})
 }
 
+func readinessEndpoint(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	if _, err := w.Write([]byte("OK")); err != nil {
+		panic(err)
+	}
+}
+
 func main() {
 	fmt.Println("vim-go")
 
+	appPrefix := "/app"
+
 	// kick off the new multiplexer
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	mux.Handle(appPrefix+"/", http.StripPrefix(appPrefix, http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", readinessEndpoint)
 
 	// wrap the mux in a custom middleware for CORS headers
 	corsMux := middlewareCors(mux)
