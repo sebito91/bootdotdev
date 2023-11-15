13743a3d63870385be9c092cfe90e4568e3047c9
Sebastian Borza <sebito91@gmail.com>
2023-11-14 23:03:32 -0600

    feat(chirpy): adding validate_chirp endpoint

--- go/chirpy/api.go
+++ go/chirpy/api.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
@@ -13,11 +15,19 @@ type apiConfig struct {
 	fileserverHits int
 }
 
+// errorBody is a struct used for returning a JSON-based error code/string
+type errorBody struct {
+	Error     string `json:"error"`
+	errorCode int
+}
+
 // GetAPI returns the router for the /api endpoint
 func (c *apiConfig) GetAPI() chi.Router {
 	r := chi.NewRouter()
 
 	r.Get("/healthz", readinessEndpoint)
+
+	r.Post("/validate_chirp", c.validateChirp)
 
 	return r
 }
@@ -68,6 +78,54 @@ func (c *apiConfig) resetEndpoint(w http.ResponseWriter, r *http.Request) {
 	c.fileserverHits = 0
 }
 
+func (c *apiConfig) validateChirp(w http.ResponseWriter, r *http.Request) {
+	type bodyCheck struct {
+		Body string `json:"body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	bodyChk := bodyCheck{}
+
+	// handle a decode error
+	if err := decoder.Decode(&bodyChk); err != nil {
+		errBody := errorBody{
+			Error:     fmt.Sprintf("%s", err),
+			errorCode: http.StatusInternalServerError,
+		}
+
+		errBody.writeErrorToPage(w)
+		return
+	}
+
+	// if chirp is too long (>140 chars), send a 400 error
+	if len(bodyChk.Body) > 140 {
+		errBody := errorBody{
+			Error:     "Chirp is too long",
+			errorCode: http.StatusBadRequest,
+		}
+
+		errBody.writeErrorToPage(w)
+		return
+	}
+
+	dat, datErr := json.Marshal(struct {
+		Valid bool `json:"valid"`
+	}{
+		Valid: true,
+	})
+	if datErr != nil {
+		log.Printf("Error marshaling JSON: %s", datErr)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	if _, wErr := w.Write(dat); wErr != nil {
+		log.Printf("Error writing JSON to page: %s", wErr)
+		return
+	}
+}
+
 // readinessEndpoint yields the status and information for the /healthz endpoint
 func readinessEndpoint(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(http.StatusOK)
@@ -75,5 +133,21 @@ func readinessEndpoint(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 	if _, err := w.Write([]byte("OK")); err != nil {
 		panic(err)
+	}
+}
+
+// writeErrorToPage is a helper function that reuses a JSON-posting for error messages
+func (e *errorBody) writeErrorToPage(w http.ResponseWriter) {
+	w.WriteHeader(e.errorCode)
+	dat, datErr := json.Marshal(e)
+	if datErr != nil {
+		log.Printf("Error marshaling JSON: %s", datErr)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	if _, wErr := w.Write(dat); wErr != nil {
+		log.Printf("Error writing JSON to page: %s", wErr)
+		return
 	}
 }
