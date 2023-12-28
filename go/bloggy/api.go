8dd97e8cd780af54b4771f4f458b835a33672f7d
Sebastian Borza <sebito91@gmail.com>
2023-12-27 23:32:39 -0600

    feat(api): initial commit, createFeed authorized endpoint

--- go/bloggy/api.go
+++ go/bloggy/api.go
@@ -47,6 +47,8 @@ func GetAPI() (*APIConfig, error) {
 
 		r.Get("/users", api.middlewareAuth(api.getUserByAPIKey))
 		r.Post("/users", api.createUser)
+
+		r.Post("/feeds", api.middlewareAuth(api.createFeed))
 	})
 
 	api.Router = r
@@ -105,4 +107,54 @@ func mainPage(w http.ResponseWriter, r *http.Request) {
 	if _, err := w.Write([]byte("we're A-OK!")); err != nil {
 		respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("failed to write: %s", err))
 	}
+}
+
+// createFeed will generate a new entry in the feeds table using the providing information
+func (api *APIConfig) createFeed(w http.ResponseWriter, r *http.Request, user database.User) {
+	type newFeedCheck struct {
+		Name string `json:"name"`
+		URL  string `json:"url"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	newFeedChk := newFeedCheck{}
+
+	if err := decoder.Decode(&newFeedChk); err != nil {
+		respondWithError(w, http.StatusBadRequest, fmt.Sprintf("createFeed: could not decode JSON payload: %s", err))
+		return
+	}
+
+	if newFeedChk.Name == "" {
+		respondWithError(w, http.StatusBadRequest, "createFeed: did not receive value for `name` field")
+		return
+	}
+
+	if newFeedChk.URL == "" {
+		respondWithError(w, http.StatusBadRequest, "createFeed: did not receive value for `url` field")
+		return
+	}
+
+	newUUID, err := uuid.NewRandom()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("createFeed: %s", err))
+		return
+	}
+
+	newTime := time.Now()
+	newFeed := database.CreateFeedParams{
+		ID:        newUUID,
+		CreatedAt: newTime,
+		UpdatedAt: newTime,
+		Name:      newFeedChk.Name,
+		Url:       newFeedChk.URL,
+		UserID:    user.ID,
+	}
+
+	feed, err := api.DB.CreateFeed(r.Context(), newFeed)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("createFeed: %s", err))
+		return
+	}
+
+	respondWithJSON(w, http.StatusCreated, feed)
 }
